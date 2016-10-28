20658ab735ad3a11f71019e35f88e3b867b8f2fb
correadaniel3 <correadaniel3@gmail.com>
2016-10-28 18:56:13 -0500

    pruebas de aceptacion

---
+++ tests/acceptance/pagina-inicial-test.js
@@ -0,0 +1,25 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'restaurant-007/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | pagina inicial');
+
+test('debe buscar restaurantes por nombre', function (assert) {
+});
+
+test('debe buscar restaurantes por ubicacion geografica.', function (assert) {
+});
+
+test('debe buscar restaurantes por puntuacion.', function (assert) {
+});
+
+test('debe buscar restaurantes por tipo de cocina.', function (assert) {
+});
+
+test('debe buscar restaurantes por rango de precios.', function (assert) {
+});
+
+test('debe buscar restaurantes de acuerdo a mi ubicacion geografica', function (assert) {
+});
+
+test('debe visualizar ubicacion geografica de restaurantes', function (assert) {
+});
